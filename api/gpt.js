92374ab184512d767b9916e9141074bc6e128462
FireNSpark <unfavorablejosh@gmail.com>
2025-08-04 00:38:13 -0400

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -132,4 +132,4 @@ export default async function handler(req, res) {
     console.error("GPT Error:", err);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
-} // ← this was missing
+}
