fd193f991d3e95022ffca4493d3a0ffe3f3aca5e
FireNSpark <unfavorablejosh@gmail.com>
2025-09-08 20:24:10 -0400

    Update health.js

--- api/health.js
+++ api/health.js
@@ -4,9 +4,6 @@ export default function handler(_req, res) {
     ok: true,
     has_OPENAI_API_KEY: !!process.env.OPENAI_API_KEY,
     has_GITHUB_TOKEN: !!process.env.GITHUB_TOKEN,
-    has_GIST_ID: !!process.env.GIST_ID,
-    has_AZURE_OPENAI_ENDPOINT: !!process.env.AZURE_OPENAI_ENDPOINT,
-    has_AZURE_OPENAI_DEPLOYMENT: !!process.env.AZURE_OPENAI_DEPLOYMENT,
-    has_AZURE_OPENAI_API_VERSION: !!process.env.AZURE_OPENAI_API_VERSION
+    has_GIST_ID: !!process.env.GIST_ID
   });
 }
